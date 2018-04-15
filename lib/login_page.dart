2426a2852e8f53afbfe677a994b63d1ab9325726
Andrea Bizzotto <bizz84@gmail.com>
2018-04-15 10:06:19 +0100

    Add registration flow part 1

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -7,12 +7,18 @@ class LoginPage extends StatefulWidget {
   State<StatefulWidget> createState() => new _LoginPageState();
 }
 
+enum FormType {
+  login,
+  register
+}
+
 class _LoginPageState extends State<LoginPage> {
 
   final formKey = new GlobalKey<FormState>();
 
   String _email;
   String _password;
+  FormType _formType = FormType.login;
 
   bool validateAndSave() {
     final form = formKey.currentState;
@@ -23,10 +29,28 @@ class _LoginPageState extends State<LoginPage> {
     return false;
   }
 
-  void validateAndSubmit() {
+  void validateAndSubmit() async {
     if (validateAndSave()) {
-      FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password);
+      try { 
+        FirebaseUser user = await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password);
+        print('Signed in: ${user.uid}');
+      }
+      catch (e) {
+        print('Error: $e');
+      }
     }
+  }
+
+  void moveToRegister() {
+    setState(() {
+      _formType = FormType.register;
+    });
+  }
+
+  void moveToLogin() {
+    setState(() {
+      _formType = FormType.login;
+    });
   }
 
   @override
@@ -41,26 +65,52 @@ class _LoginPageState extends State<LoginPage> {
             key: formKey,
             child: new Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                new TextFormField(
-                  decoration: new InputDecoration(labelText: 'Email'),
-                  validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
-                  onSaved: (value) => _email = value,
-                ),
-                new TextFormField(
-                  decoration: new InputDecoration(labelText: 'Password'),
-                  obscureText: true,
-                  validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
-                  onSaved: (value) => _password = value,
-                ),
-                new RaisedButton(
-                  child: new Text('Login', style: new TextStyle(fontSize: 20.0)),
-                  onPressed: validateAndSubmit,
-                )
-              ],
+              children: buildInputs() + buildSubmitButtons(),
             ),
           )
         )
       );
+    }
+
+    List<Widget> buildInputs() {
+      return [
+        new TextFormField(
+          decoration: new InputDecoration(labelText: 'Email'),
+          validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+          onSaved: (value) => _email = value,
+        ),
+        new TextFormField(
+          decoration: new InputDecoration(labelText: 'Password'),
+          obscureText: true,
+          validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+          onSaved: (value) => _password = value,
+        ),
+      ];
+    }
+
+    List<Widget> buildSubmitButtons() {
+      if (_formType == FormType.login) {
+        return [
+          new RaisedButton(
+            child: new Text('Login', style: new TextStyle(fontSize: 20.0)),
+            onPressed: validateAndSubmit,
+          ),
+          new FlatButton(
+            child: new Text('Create an account', style: new TextStyle(fontSize: 20.0)),
+            onPressed: moveToRegister,
+          ),
+        ];
+      } else {
+        return [
+          new RaisedButton(
+            child: new Text('Create an account', style: new TextStyle(fontSize: 20.0)),
+            onPressed: validateAndSubmit,
+          ),
+          new FlatButton(
+            child: new Text('Have an account? Login', style: new TextStyle(fontSize: 20.0)),
+            onPressed: moveToLogin,
+          ),
+        ];
+      }
     }
 }
